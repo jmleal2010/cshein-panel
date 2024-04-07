5b10fdccd273d25b7c1ad2385b226bcfd495ba9c
Juan Miguel <jmleal2010@gmail.com>
2024-04-07 21:54:13 +0200

    Setting new ui

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,65 +1,88 @@
-'use client'
-import "../globals.css";
-import Navbar from "@/components/common/Navbar";
-import {Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar} from "@mui/material";
 
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import * as React from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Box from '@mui/joy/Box';
+import Button from '@mui/joy/Button';
+import Breadcrumbs from '@mui/joy/Breadcrumbs';
+import Link from '@mui/joy/Link';
+import Typography from '@mui/joy/Typography';
 
-export default function DashboardLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
+
+import Sidebar from '@/components/common/Sidebar';
+import OrderTable from '@/components/common/OrderTable';
+import OrderList from '@/components/common/OrderList';
+import Header from '@/components/common/Header';
+
+import '@fontsource/inter';
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
+    return (
 
-    const drawerWidth = 240;
+        <CssVarsProvider disableTransitionOnChange>
+            <CssBaseline />
+            <Box sx={{ display: 'flex', minHeight: '100dvh' }}>
+                <Header />
+                <Sidebar />
+                <Box
+                    component="main"
+                    className="MainContent"
+                    sx={{
+                        px: { xs: 2, md: 6 },
+                        pt: {
+                            xs: 'calc(12px + var(--Header-height))',
+                            sm: 'calc(12px + var(--Header-height))',
+                            md: 3,
+                        },
+                        pb: { xs: 2, sm: 2, md: 3 },
+                        flex: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        minWidth: 0,
+                        height: '100dvh',
+                        gap: 1,
+                    }}
+                >
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Breadcrumbs
+                            size="sm"
+                            aria-label="breadcrumbs"
+                            separator={<ChevronRightRoundedIcon fontSize="sm" />}
+                            sx={{ pl: 0 }}
+                        >
+                            <Link
+                                underline="none"
+                                color="neutral"
+                                href="#some-link"
+                                aria-label="Home"
+                            >
+                                <HomeRoundedIcon />
+                            </Link>
+                            <Link
+                                underline="hover"
+                                color="neutral"
+                                href="#some-link"
+                                fontSize={12}
+                                fontWeight={500}
+                            >
+                                Dashboard
+                            </Link>
+                            <Typography color="primary" fontWeight={500} fontSize={12}>
+                                Orders
+                            </Typography>
+                        </Breadcrumbs>
+                    </Box>
 
-  return (
-    <>
-        <Drawer
-            sx={{
-                width: drawerWidth,
-                flexShrink: 0,
-                '& .MuiDrawer-paper': {
-                    width: drawerWidth,
-                    boxSizing: 'border-box',
-                },
-            }}
-            variant="permanent"
-            anchor="left"
-        >
-
-            <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                    <ListItem key={text} disablePadding >
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-            <List>
-                {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                            </ListItemIcon>
-                            <ListItemText primary={text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </Drawer>
-        <main >
-            <div className="">
-                {children}
-            </div>
-        </main>
-    </>
-  );
+                    {children}
+                </Box>
+            </Box>
+        </CssVarsProvider>
+    );
 }
--- components/common/Sidebar.tsx
+++ components/common/Sidebar.tsx
@@ -1,210 +1,246 @@
-"use client";
-import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
-import {
-  faCog,
-  faHome,
-  faUsers,
-  faXmark,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+'use client'
+import * as React from 'react';
+import Avatar from '@mui/joy/Avatar';
+import Box from '@mui/joy/Box';
+import Divider from '@mui/joy/Divider';
+import IconButton from '@mui/joy/IconButton';
+import Input from '@mui/joy/Input';
+import List from '@mui/joy/List';
+import ListItem from '@mui/joy/ListItem';
+import ListItemButton, {listItemButtonClasses} from '@mui/joy/ListItemButton';
+import ListItemContent from '@mui/joy/ListItemContent';
+import Typography from '@mui/joy/Typography';
+import Sheet from '@mui/joy/Sheet';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
+import ShoppingCartRoundedIcon from '@mui/icons-material/ShoppingCartRounded';
+import AssignmentRoundedIcon from '@mui/icons-material/AssignmentRounded';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import BrightnessAutoRoundedIcon from '@mui/icons-material/BrightnessAutoRounded';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+import ColorSchemeToggle from './ColorSchemeToggle';
+import {closeSidebar} from '@/utils';
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import {routes} from "@/config/consts";
 
-const navigation = [
-  { name: "Dashboard", href: routes.dashboard, icon: faHome, current: true },
-  { name: "Ordenes", href: routes.orders, icon: faUsers, current: false },
-];
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
+function Toggler({
+                     defaultExpanded = false,
+                     renderToggle,
+                     children,
+                 }: {
+    defaultExpanded?: boolean;
+    children: React.ReactNode;
+    renderToggle: (params: {
+        open: boolean;
+        setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    }) => React.ReactNode;
+}) {
+    const [open, setOpen] = React.useState(defaultExpanded);
+    return (
+        <React.Fragment>
+            {renderToggle({open, setOpen})}
+            <Box
+                sx={{
+                    display: 'grid',
+                    gridTemplateRows: open ? '1fr' : '0fr',
+                    transition: '0.2s ease',
+                    '& > *': {
+                        overflow: 'hidden',
+                    },
+                }}
+            >
+                {children}
+            </Box>
+        </React.Fragment>
+    );
 }
 
+const navigation = [
+    {
+        title: 'Inicio',
+        icon: <DashboardRoundedIcon/>,
+        children: [],
+        nested: false,
+        href: routes.dashboard
+    },
+    {
+        title: "Ordenes",
+        icon: <ShoppingCartRoundedIcon/>,
+        children: [],
+        nested: false,
+        href: routes.orders
+    },
+    {
+        title: 'Tareas',
+        icon: <AssignmentRoundedIcon/>,
+        children: [
+            'Primera',
+            'Segunda',
+            'Tercera'
+        ],
+        nested: true,
+        href: routes.orders
+    }
+]
+
 export default function Sidebar() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const pathname = usePathname();
-
-  const activeRoute = (route: string) => {
-    return pathname === route;
-  };
-
-  return (
-    <>
-      <Transition.Root show={sidebarOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-50 lg:hidden"
-          onClose={setSidebarOpen}
+    return (
+        <Sheet
+            className="Sidebar"
+            sx={{
+                position: {xs: 'fixed', md: 'sticky'},
+                transform: {
+                    xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1)))',
+                    md: 'none',
+                },
+                transition: 'transform 0.4s, width 0.4s',
+                zIndex: 10000,
+                height: '100dvh',
+                width: 'var(--Sidebar-width)',
+                top: 0,
+                p: 2,
+                flexShrink: 0,
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+                borderRight: '1px solid',
+                borderColor: 'divider',
+            }}
         >
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-gray-900/80" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 flex">
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
-              <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-in-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                    <button
-                      type="button"
-                      className="-m-2.5 p-2.5"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <FontAwesomeIcon
-                        icon={faXmark}
-                        className="h-6 w-6 text-white"
-                        aria-hidden="true"
-                      />
-                    </button>
-                  </div>
-                </Transition.Child>
-                {/* Sidebar component, swap this element with another sidebar if you like */}
-                <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
-                  <div className="flex h-16 shrink-0 items-center">
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt="Your Company"
-                    />
-                  </div>
-                  <nav className="flex flex-1 flex-col">
-                    <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                      <li>
-                        <ul role="list" className="-mx-2 space-y-1">
-                          {navigation.map((item) => (
-                            <li key={item.name}>
-                              <Link
-                                href={item.href}
-                                className={classNames(
-                                  activeRoute(item.href)
-                                    ? "bg-gray-50 text-indigo-600"
-                                    : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                                  "link group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                                )}
-                              >
-                                <FontAwesomeIcon
-                                  icon={item.icon}
-                                  className={classNames(
-                                    activeRoute(item.href)
-                                      ? "text-indigo-600"
-                                      : "text-gray-400 group-hover:text-indigo-600",
-                                    "h-6 w-6 shrink-0"
-                                  )}
-                                  aria-hidden="true"
-                                />
-                                {item.name}
-                              </Link>
-                            </li>
-                          ))}
-                        </ul>
-                      </li>
-                      <li className="mt-auto">
-                        <Link
-                          href="#"
-                          className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600"
-                        >
-                          <FontAwesomeIcon
-                            icon={faCog}
-                            className="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
-                            aria-hidden="true"
-                          />
-                          Settings
-                        </Link>
-                      </li>
-                    </ul>
-                  </nav>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </Dialog>
-      </Transition.Root>
-
-      {/* Static sidebar for desktop */}
-      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
-        {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
-          <div className="flex h-16 shrink-0 items-center">
-            <img
-              className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
+            {/* <GlobalStyles
+        styles={(theme) => ({
+          ':root': {
+            '--Sidebar-width': '220px',
+            [theme.breakpoints.up('lg')]: {
+              '--Sidebar-width': '240px',
+            },
+          },
+        })}
+      />*/}
+            <Box
+                className="Sidebar-overlay"
+                sx={{
+                    position: 'fixed',
+                    zIndex: 9998,
+                    top: 0,
+                    left: 0,
+                    width: '100vw',
+                    height: '100vh',
+                    opacity: 'var(--SideNavigation-slideIn)',
+                    backgroundColor: 'var(--joy-palette-background-backdrop)',
+                    transition: 'opacity 0.4s',
+                    transform: {
+                        xs: 'translateX(calc(100% * (var(--SideNavigation-slideIn, 0) - 1) + var(--SideNavigation-slideIn, 0) * var(--Sidebar-width, 0px)))',
+                        lg: 'translateX(-100%)',
+                    },
+                }}
+                onClick={() => closeSidebar()}
             />
-          </div>
-          <nav className="flex flex-1 flex-col">
-            <ul role="list" className="flex flex-1 flex-col gap-y-7">
-              <li>
-                <ul role="list" className="-mx-2 space-y-1">
-                  {navigation.map((item) => (
-                    <li key={item.name}>
-                      <Link
-                        href={item.href}
-                        className={classNames(
-                          activeRoute(item.href)
-                            ? "bg-gray-50 text-indigo-600"
-                            : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-                          "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                        )}
-                      >
-                        <FontAwesomeIcon
-                          icon={item.icon}
-                          className={classNames(
-                            activeRoute(item.href)
-                              ? "text-indigo-600"
-                              : "text-gray-400 group-hover:text-indigo-600",
-                            "h-6 w-6 shrink-0"
-                          )}
-                          aria-hidden="true"
-                        />
-                        {item.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-              <li className="mt-auto">
-                <Link
-                  href="#"
-                  className="group -mx-2 flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600"
+            <Box sx={{display: 'flex', gap: 1, alignItems: 'center'}}>
+                <IconButton variant="soft" color="primary" size="sm">
+                    <BrightnessAutoRoundedIcon/>
+                </IconButton>
+                <Typography level="title-lg">Acme Co.</Typography>
+                <ColorSchemeToggle sx={{ml: 'auto'}}/>
+            </Box>
+            <Input size="sm" startDecorator={<SearchRoundedIcon/>} placeholder="Search"/>
+            <Box
+                sx={{
+                    minHeight: 0,
+                    overflow: 'hidden auto',
+                    flexGrow: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    [`& .${listItemButtonClasses.root}`]: {
+                        gap: 1.5,
+                    },
+                }}
+            >
+                <List
+                    size="sm"
+                    sx={{
+                        gap: 1,
+                        '--List-nestedInsetStart': '30px',
+                        '--ListItem-radius': (theme) => theme.vars.radius.sm,
+                    }}
                 >
-                  <FontAwesomeIcon
-                    icon={faCog}
-                    className="h-6 w-6 shrink-0 text-gray-400 group-hover:text-indigo-600"
-                    aria-hidden="true"
-                  />
-                  Settings
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </>
-  );
+                    {navigation.map((item, index) => !item.nested ? (
+                        <Link className="no-underline" href={item.href} style={{textDecoration: 'none'}}>
+                            <ListItem key={index}>
+                                <ListItemButton>
+                                    {item.icon}
+
+                                    <ListItemContent>
+                                        <Typography level="title-sm">{item.title}</Typography>
+                                    </ListItemContent>
+
+                                </ListItemButton>
+                            </ListItem>
+                        </Link>
+                    ) : (
+                        <ListItem key={index} nested>
+                            <Toggler
+                                renderToggle={({open, setOpen}) => (
+                                    <ListItemButton onClick={() => setOpen(!open)}>
+                                        <AssignmentRoundedIcon/>
+                                        <ListItemContent>
+                                            <Typography level="title-sm">Tasks</Typography>
+                                        </ListItemContent>
+                                        <KeyboardArrowDownIcon
+                                            sx={{transform: open ? 'rotate(180deg)' : 'none'}}
+                                        />
+                                    </ListItemButton>
+                                )}
+                            >
+                                <List sx={{gap: 0.5}}>
+                                    {item.children.map((child, index) => (
+                                        <ListItem sx={{mt: 0.5}} key={index}>
+                                            <ListItemButton>{child}</ListItemButton>
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            </Toggler>
+                        </ListItem>
+                    ))}
+
+
+                </List>
+
+                <List
+                    size="sm"
+                    sx={{
+                        mt: 'auto',
+                        flexGrow: 0,
+                        '--ListItem-radius': (theme) => theme.vars.radius.sm,
+                        '--List-gap': '8px',
+                        mb: 2,
+                    }}
+                >
+                    <ListItem>
+                        <ListItemButton>
+                            <SettingsRoundedIcon/>
+                            Settings
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </Box>
+            <Divider/>
+            <Box sx={{display: 'flex', gap: 1, alignItems: 'center'}}>
+                <Avatar
+                    variant="outlined"
+                    size="sm"
+                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
+                />
+                <Box sx={{minWidth: 0, flex: 1}}>
+                    <Typography level="title-sm">Siriwat K.</Typography>
+                    <Typography level="body-xs">siriwatk@test.com</Typography>
+                </Box>
+                <IconButton size="sm" variant="plain" color="neutral">
+                    <LogoutRoundedIcon/>
+                </IconButton>
+            </Box>
+        </Sheet>
+    );
 }
