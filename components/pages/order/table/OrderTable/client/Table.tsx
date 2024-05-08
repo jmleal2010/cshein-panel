8907250a7941f80544460f0946c9d10d0c7ec06a
Juan Miguel <jmleal2010@gmail.com>
2024-05-09 01:46:57 +0200

    saved develop

--- components/pages/order/table/OrderTable/client/Table.tsx
+++ components/pages/order/table/OrderTable/client/Table.tsx
@@ -13,11 +13,19 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  Modal,
+  Box,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  FormHelperText,
 } from "@mui/material";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import moment, { Moment } from "moment";
 import { height } from "@fortawesome/free-brands-svg-icons/fa42Group";
-import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faPencil } from "@fortawesome/free-solid-svg-icons";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 const WAIT_BETWEEN_CHANGE = 1000;
 
@@ -34,6 +42,9 @@ export default function Table({
 }) {
   /*States*/
   const [page, setPage] = React.useState<number>(0);
+   const [open, setOpen] = React.useState(false);
+   const handleOpen = () => setOpen(true);
+   const handleClose = () => setOpen(false);
 
   /* Hooks */
   const searchParams = useSearchParams();
@@ -46,14 +57,56 @@ export default function Table({
     router.push(`${pathname}/${id}`);
   };
 
+  const onOrderEdit = (id: string) => { 
+
+  }
+
   const visibleRows = React.useMemo(
     () => rows.slice(page * ITEMS_X_PAGE, page * ITEMS_X_PAGE + ITEMS_X_PAGE),
     [rows, page]
   );
 
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [formData, setFormData] = React.useState({
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+    showPassword: false,
+  });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setFormData({ ...formData, showPassword: !formData.showPassword });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aquí puedes manejar la lógica para enviar el formulario
+    console.log(formData);
+  };
+
   return (
     <React.Fragment>
-      <TableContainer  component={Paper} style={{ marginTop: 25 }}>
+      <TableContainer component={Paper} style={{ marginTop: 25 }}>
         <MTable sx={{ minWidth: 650 }} aria-label="order table" size="small">
           <TableHead>
             <TableRow>
@@ -63,7 +116,7 @@ export default function Table({
                   key={index}
                   sx={{
                     pb: 2,
-                    pt:1
+                    pt: 1,
                   }}
                 >
                   <Typography
@@ -112,12 +165,92 @@ export default function Table({
                   <IconButton onClick={() => onViewOrder(row.id)}>
                     <FontAwesomeIcon size="xs" icon={faEye} />
                   </IconButton>
+                  <IconButton onClick={handleOpen}>
+                    <FontAwesomeIcon size="xs" icon={faPencil} />
+                  </IconButton>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </MTable>
-        </TableContainer>
+      </TableContainer>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <form onSubmit={handleSubmit}>
+            <FormControl fullWidth sx={{ my: 1 }} variant="outlined" required>
+              <InputLabel htmlFor="name">Name</InputLabel>
+              <OutlinedInput
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                label="Name"
+              />
+            </FormControl>
+            <FormControl fullWidth sx={{ my: 1 }} variant="outlined" required>
+              <InputLabel htmlFor="lastName">Last Name</InputLabel>
+              <OutlinedInput
+                id="lastName"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+                label="Last Name"
+              />
+            </FormControl>
+            <FormControl fullWidth sx={{ my: 1 }} variant="outlined" required>
+              <InputLabel htmlFor="email">Email</InputLabel>
+              <OutlinedInput
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                label="Email"
+              />
+            </FormControl>
+            <FormControl fullWidth sx={{ my: 1 }} variant="outlined" required>
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <OutlinedInput
+                id="password"
+                name="password"
+                type={formData.showPassword ? "text" : "password"}
+                value={formData.password}
+                onChange={handleChange}
+                label="Password"
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {formData.showPassword ? (
+                        <VisibilityOff />
+                      ) : (
+                        <Visibility />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+              <FormHelperText error={true}>Password is required</FormHelperText>
+            </FormControl>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              sx={{ mt: 2 }}
+            >
+              Submit
+            </Button>
+          </form>
+        </Box>
+      </Modal>
     </React.Fragment>
   );
 }
